ffdd9f506daa4e46fba8af4805701e9519eeefe3
DenissePezoa <denisse.pezoa@gmail.com>
2020-05-03 16:37:19 -0400

    Prototipo de manejo de archivo PDF, crear y modificar.

---
+++ Prototipo3-ManejoPDF/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Prototipo3_ManejoPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Crear y Guardar PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 67);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Editar y Guadar PDF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(336, 22);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(422, 406);
+            this.axAcroPDF1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingresar texto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Coordenadas de rectangulo a dibujar";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "10";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(102, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(34, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "100";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(217, 107);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(37, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "50";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(217, 81);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(37, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "100";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Esquina superior X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Esquina superior Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(147, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Height (alto)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(142, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Widht (ancho)";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(150, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 44);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Obtener texto de PDF";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Nombre Archivo";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(90, 23);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(176, 20);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.Text = "Code.pdf";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(9, 216);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(303, 212);
+            this.panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(9, 22);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(302, 165);
+            this.panel2.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 200);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Editar PDF existente";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 4);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Crear PDF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 440);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Name = "Form1";
+            this.Text = "Prototipo Manejo de PDF";
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Prototipo3-ManejoPDF/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+namespace Prototipo3_ManejoPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document document = new Document(iTextSharp.text.PageSize.A4);
+           // document.PageSize.BackgroundColor = new iTextSharp.text.BaseColor(255,255,255);
+              
+            document.PageSize.Rotate();
+            //creamos un instancia del objeto escritor de documento
+            PdfWriter writer = PdfWriter.GetInstance(document, new System.IO.FileStream
+            ("Code.pdf", System.IO.FileMode.Create));
+
+            //definimos la manera de inicialización de abierto del documento.
+            //esto, hará que veamos al inicio, todas la páginas del documento
+            //en la parte izquierda
+            writer.ViewerPreferences = PdfWriter.PageModeUseThumbs;
+            
+
+            //abrimos el documento para agregarle contenido
+            document.Open();
+
+            //creamos la fuente
+            iTextSharp.text.Font myfont = new iTextSharp.text.Font(
+            FontFactory.GetFont(FontFactory.COURIER, 10, iTextSharp.text.Font.ITALIC));
+
+            Paragraph myParagraph = new Paragraph("Primera prueba ziiii \n", myfont);
+            myParagraph.Add(new Paragraph(textBox1.Text, myfont));
+            //agregar todo el paquete de texto
+            document.Add(myParagraph);
+           
+            //esto es importante, pues si no cerramos el document entonces no se creara el pdf.
+            document.Close();
+
+            //esto es para abrir el documento y verlo inmediatamente después de su creación
+            //System.Diagnostics.Process.Start("AcroRd32.exe", "Code.pdf");
+
+            //Disable el boton y habilitar el otro
+            MessageBox.Show("Pdf creado con exito ");
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            string oldFile = textBox6.Text;
+            string newFile = "temp777.pdf";
+            
+            PdfReader reader = new PdfReader(oldFile);
+            
+
+            PdfStamper stamper = new PdfStamper(reader, new FileStream(newFile, FileMode.Append));
+            //Image img = Image.getInstance(IMG);
+            float x = Convert.ToSingle(textBox2.Text);
+            float y = Convert.ToSingle(textBox3.Text);
+            float w = Convert.ToSingle(textBox5.Text);
+            float h = Convert.ToSingle(textBox4.Text);
+
+            PdfContentByte contentunder = stamper.GetUnderContent(1);
+            contentunder.SetColorStroke(BaseColor.RED);
+            contentunder.Rectangle(x, y, w, h);
+            //axAcroPDF1.RectangleToScreen(new System.Drawing.Rectangle(10, 10, 20, 100));
+            contentunder.Stroke();
+
+
+            //img.setAbsolutePosition(x, y);
+            //stamper.getOverContent(1).addImage(img);
+            /*iTextSharp.text.Rectangle linkLocation = new iTextSharp.text.Rectangle(x, y, x + w, y + h);
+            PdfDestination destination = new PdfDestination(PdfDestination.FIT);
+            PdfAnnotation link = PdfAnnotation.CreateLink(stamper.Writer,
+                    linkLocation, PdfAnnotation.HIGHLIGHT_INVERT,
+                    1 , destination);
+            PdfBorderArray border =(new PdfBorderArray(0, 0, 0));
+            link.Border = border;
+            stamper.AddAnnotation(link, 1);*/
+
+            
+            
+            stamper.Close();
+            reader.Close();
+            File.Replace(@newFile, oldFile, @"backup.pdf.bac");
+            MessageBox.Show("Pdf modificado con exito, se ha guardado un backup de la versión anterior ");
+
+            axAcroPDF1.src = "C:\\Users\\Denisse\\Desktop\\prototipos\\Prototipo3-ManejoPDF\\Prototipo3-ManejoPDF\\bin\\Debug\\"+oldFile;
+            /*
+            // open the reader
+            PdfReader reader = new PdfReader(oldFile);
+            iTextSharp.text.Rectangle size = reader.GetPageSizeWithRotation(1);
+            Document document = new Document(size);
+
+            // open the writer
+            
+            
+            FileStream fs = new FileStream(newFile, FileMode.Create, FileAccess.Write);
+            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            //PdfWriter writer = PdfWriter.GetInstance(document, new System.IO.FileStream
+            //("Code.pdf", System.IO.FileMode.Create));
+            document.Open();
+
+            // the pdf content
+            PdfContentByte cb = writer.DirectContent;
+
+            // select the font properties
+            BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            cb.SetColorFill(BaseColor.DARK_GRAY);
+            cb.SetFontAndSize(bf, 8);
+
+            // write the text in the pdf content
+            cb.BeginText();
+            string text = "Some random blablablabla...";
+            // put the alignment and coordinates here
+            cb.ShowTextAligned(1, text, 520, 640, 0);
+            cb.EndText();
+            cb.BeginText();
+            text = "Other random blabla...";
+            // put the alignment and coordinates here
+            cb.ShowTextAligned(2, text, 100, 200, 0);
+            cb.EndText();
+
+            // create the new page and add it to the pdf
+            PdfImportedPage page = writer.GetImportedPage(reader, 1);
+            cb.AddTemplate(page, 0, 0);
+
+            // close the streams and voilá the file should be changed :)
+            document.Close();
+            fs.Close();
+            writer.Close();
+            reader.Close();*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*string src = "ejemplo.pdf";
+            string dest = "Ejemplo4.pdf";
+
+            File.Copy(src,dest);
+           
+            
+                MessageBox.Show("Pdf copiado con exito");*/
+            String archivo = textBox6.Text;
+
+            PdfReader inputDocument = new PdfReader(archivo);
+
+            StringBuilder text = new StringBuilder();
+
+            for (int page = 1; page <= inputDocument.NumberOfPages; page++)
+
+            {
+
+
+
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+
+                string currentText = PdfTextExtractor.GetTextFromPage(inputDocument, page, strategy);
+
+
+
+                currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
+
+                text.Append(currentText);
+
+            }
+
+            inputDocument.Close();
+
+            MessageBox.Show(text.ToString());
+
+        }
+    }
+}
